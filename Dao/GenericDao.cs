d8dc3323555b587fd84e1d4e87a40744f226bb6a
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-06-08 09:54:35 +0700

    update code

--- Dao/GenericDao.cs
+++ Dao/GenericDao.cs
@@ -1,6 +1,39 @@
-﻿namespace Dao;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
 
-public class GenericDao
+namespace Dao;
+
+public class GenericDao<T> where T : class
 {
-    
+   private readonly GiveAwayDbContext _context;
+
+   public GenericDao(GiveAwayDbContext context)
+   {
+      _context = context;
+   }
+
+   public IQueryable<T> GetQueryable()
+   {
+      return _context.Set<T>().AsQueryable();
+   }
+   
+   public  void AddAsync(T entity)
+   {
+      _context.Set<T>().Add(entity);
+   }
+   
+   public  void UpdateAsync(T entity)
+   {
+      _context.Set<T>().Update(entity);
+   }
+   
+   public void  DeleteAsync(T entity)
+   {
+      _context.Set<T>().Remove(entity);
+   }
+   
+   public Task SaveChangesAsync()
+   {
+      return _context.SaveChangesAsync();
+   }
+  
 }
