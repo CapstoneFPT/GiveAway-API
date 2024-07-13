ecc5b5e073e91cd43a1075502d35385b53fa244e
PhucLe <lnphuc2006@gmail.com>
2024-07-13 13:21:18 +0700

    update db

--- BusinessObjects/Entities/Member.cs
+++ BusinessObjects/Entities/Member.cs
@@ -8,7 +8,7 @@ public class Member : Account
     public ICollection<Order> Orders = new List<Order>();
     public ICollection<Bid> Bids = new List<Bid>();
     public ICollection<AuctionDeposit> AuctionDeposits = new List<AuctionDeposit>();
-    public ICollection<Refund> Refunds = new List<Refund>();
+    
     public ICollection<Feedback> Feedbacks = new List<Feedback>();
     public ICollection<Transaction> Transactions = new List<Transaction>();
 }
