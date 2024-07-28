1af2f8cf7c76fbc1c694cf1e17d44e04dee43e00
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-07-28 12:42:41 +0700

    added input validation for requesting withdraws

--- BusinessObjects/Dtos/Withdraws/CreateWithdrawRequest.cs
+++ BusinessObjects/Dtos/Withdraws/CreateWithdrawRequest.cs
@@ -1,10 +1,17 @@
-﻿namespace BusinessObjects.Dtos.Withdraws;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessObjects.Dtos.Withdraws;
 
 public class CreateWithdrawRequest
 {
+    [Required]
+    [Range(0,int.MaxValue,ErrorMessage = "Amount must be greater than 0")]
     public int Amount { get; set; }
 
+    [Required]
     public string Bank { get; set; } = default!;
+    [Required]
     public string BankAccountNumber { get; set; } = default!;
+    [Required]
     public string BankAccountName { get; set; } = default!;
 }
