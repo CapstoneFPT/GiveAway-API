a88f78e362ba22680c4263592732d1ceedea7173
PhucLe <lnphuc2006@gmail.com>
2024-07-29 12:07:14 +0700

    update mail service

--- Services/Emails/IEmailService.cs
+++ Services/Emails/IEmailService.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessObjects.Dtos.Commons;
 
 namespace Services.Emails
 {
     public interface IEmailService
     {
         Task SendEmail(SendEmailRequest request);
+        Task<Result<string>> SendMailRegister(string mail, string token);
     }
 }
