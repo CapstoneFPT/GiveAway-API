d507b20eec5891e92cf018dbe84c93764771fc16
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-07-29 12:20:21 +0700

    implemented get timeslot list

---
+++ BusinessObjects/Dtos/Timeslots/GetTimeslotsRequest.cs
@@ -0,0 +1,10 @@
+﻿using BusinessObjects.Dtos.Commons;
+
+namespace BusinessObjects.Dtos.Timeslots;
+
+public class GetTimeslotsRequest
+{
+    public int? PageNumber { get; set; }
+    public int? PageSize { get; set; }
+    public TimeSlotStatus? Status { get; set; }
+}
--- Repositories/Timeslots/ITimeslotRepository.cs
+++ Repositories/Timeslots/ITimeslotRepository.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessObjects.Entities;
 
 namespace Repositories.Timeslots
 {
     public interface ITimeslotRepository
     {
+        Task<(List<T> Items, int Page, int PageSize, int Total)> GetTimeslotProjections<T>(int? requestPageNumber,
+            int? requestPageSize, Expression<Func<Timeslot, bool>> predicate, Expression<Func<Timeslot, T>> selector);
     }
 }
