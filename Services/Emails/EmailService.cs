c7e0bfad4d9f13636fd9d331e39883de08fd6b45
PhucLe <lnphuc2006@gmail.com>
2024-07-31 16:31:17 +0700

    update

--- Services/Emails/EmailService.cs
+++ Services/Emails/EmailService.cs
@@ -33,7 +33,7 @@ namespace Services.Emails
         {
             string pathLocal = Path.Combine("C:\\FPT_University_FULL\\CAPSTONE_API\\Services\\MailTemplate\\", $"{templateName}.html");
             string path = Path.Combine("Services/MailTemplate/", $"{templateName}.html");
-            return File.ReadAllText(pathLocal, Encoding.UTF8);
+            return File.ReadAllText(path, Encoding.UTF8);
         }
 
         public async Task SendEmail(SendEmailRequest request)
