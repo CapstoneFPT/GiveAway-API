67bcea0bb8887b155d95f3b23045b08bf6ac4ce4
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-08-04 11:24:27 +0700

    creating new error response

---
+++ BusinessObjects/Dtos/Commons/ErrorResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+namespace BusinessObjects.Dtos.Commons;
+
+public class ErrorResponse
+{
+    public ErrorType Type { get; set; }
+    public string Message { get; set; }
+    public HttpStatusCode StatusCode { get; set; }
+    public ErrorCode ErrorCode { get; set; }
+}
+
+public enum ErrorCode
+{
+    
+}
+
+public enum ErrorType
+{
+    ApiError,
+    InvalidRequestError
+}
