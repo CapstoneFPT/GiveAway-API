ba199525025b1c9c108604e5df0023e70cf223e2
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-10-08 10:46:35 +0700

    implemented get accounts feature

---
+++ WebApi2.0/Features/Admin/Accounts/GetAccounts/GetAccountsEndpoint.cs
@@ -0,0 +1,86 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using WebApi2._0.Common;
+using WebApi2._0.Domain.Entities;
+using WebApi2._0.Domain.Enums;
+using WebApi2._0.Domain.ValueObjects;
+using WebApi2._0.Infrastructure.Persistence;
+
+namespace WebApi2._0.Features.Admin.Accounts.GetAccounts;
+
+public sealed class GetAccountsEndpoint : Endpoint<GetAccountsRequest, PaginationResponse<AccountsListResponse>>
+{
+    private readonly GiveAwayDbContext _dbContext;
+
+    public GetAccountsEndpoint(GiveAwayDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public override void Configure()
+    {
+        Get("api/accounts");
+        Roles("Admin");
+    }
+
+    public override async Task<PaginationResponse<AccountsListResponse>> ExecuteAsync(GetAccountsRequest req,
+        CancellationToken ct)
+    {
+        var query = _dbContext.Accounts.AsQueryable();
+
+        query = query.Where(GetAccountsPredicate.GetPredicate(req));
+        var count = await query.CountAsync(ct);
+
+        var data = await query
+            .OrderBy(x => x.Fullname)
+            .Skip(PaginationUtils.GetSkip(req.Page, req.PageSize))
+            .Take(PaginationUtils.GetTake(req.PageSize))
+            .Select(x => new AccountsListResponse
+            {
+                AccountId = x.AccountId,
+                Email = x.Email,
+                Phone = x.Phone,
+                Fullname = x.Fullname,
+                Role = x.Role,
+                Balance = x.Balance,
+                Status = x.Status,
+                ShopId = (x as Staff).Shop.ShopId,
+                ShopCode = (x as Staff).Shop.ShopCode
+            })
+            .ToListAsync(ct);
+
+        return new PaginationResponse<AccountsListResponse>()
+        {
+            Items = data,
+            PageNumber = req.Page ?? 1,
+            PageSize = req.PageSize ?? int.MaxValue,
+            TotalCount = count
+        };
+    }
+}
+
+public record GetAccountsRequest
+{
+    public int? Page { get; set; }
+    public int? PageSize { get; set; }
+    public string? Fullname { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public Roles[]? Roles { get; set; }
+    public AccountStatus[]? Status { get; set; } = [];
+}
+
+public record AccountsListResponse
+{
+    public Guid AccountId { get; set; }
+    [EmailAddress] public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Fullname { get; set; }
+    public Roles Role { get; set; }
+    public decimal Balance { get; set; }
+    public AccountStatus Status { get; set; }
+    public Guid? ShopId { get; set; }
+    public string? ShopCode { get; set; }
+}
---
+++ WebApi2.0/Features/Admin/Accounts/GetAccounts/GetAccountsPredicate.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq.Expressions;
+using LinqKit;
+using Microsoft.EntityFrameworkCore;
+using WebApi2._0.Domain.Entities;
+
+namespace WebApi2._0.Features.Admin.Accounts.GetAccounts;
+
+public static class GetAccountsPredicate
+{
+    public static Expression<Func<Account, bool>> GetPredicate(GetAccountsRequest req)
+    {
+        var predicates = new Dictionary<
+            Func<GetAccountsRequest, bool>,
+            Func<GetAccountsRequest, Expression<Func<Account, bool>>>
+        >()
+        {
+            [x=>x.Fullname != null] = GetAccountFullnamePredicate,
+            [x=>x.Roles is {Length: > 0}] = GetAccountRolePredicate,
+            [x=>x.Status is {Length: > 0}] = GetAccountStatusPredicate,
+            [x=>x.Email != null] = GetAccountEmailPredicate,
+            [x=>x.Phone != null] = GetAccountPhonePredicate
+        };
+
+        return predicates
+            .Where(x => x.Key(req) == true)
+            .Select(x => x.Value(req))
+            .Aggregate(PredicateBuilder.New<Account>(true), (current, next) => current.And(next));
+    }
+
+    private static Expression<Func<Account, bool>> GetAccountPhonePredicate(GetAccountsRequest arg)
+    {
+        return x => EF.Functions.ILike(x.Phone, $"%{arg.Phone}%");
+    }
+
+    private static Expression<Func<Account, bool>> GetAccountEmailPredicate(GetAccountsRequest arg)
+    {
+        return x => EF.Functions.ILike(x.Email, $"%{arg.Email}%");
+    }
+
+    private static Expression<Func<Account, bool>> GetAccountStatusPredicate(GetAccountsRequest arg)
+    {
+        return x=> arg.Status != null && arg.Status.Contains(x.Status);
+    }
+
+    private static Expression<Func<Account, bool>> GetAccountRolePredicate(GetAccountsRequest arg)
+    {
+        return x => arg.Roles != null && arg.Roles.Contains(x.Role);
+    }
+
+    private static Expression<Func<Account, bool>> GetAccountFullnamePredicate(GetAccountsRequest arg)
+    {
+        return x=> EF.Functions.ILike(x.Fullname, $"%{arg.Fullname}%");
+    }
+}
